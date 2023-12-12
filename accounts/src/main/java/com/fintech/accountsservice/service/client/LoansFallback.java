051874e0444325f173e393366a04113a71528b20
ShivamWadkar <shivamvwadkar@gmail.com>
2023-12-12 22:13:38 +0530

    Implementing Circuit Breaker pattern with Feign Client

---
+++ accounts/src/main/java/com/fintech/accountsservice/service/client/LoansFallback.java
@@ -0,0 +1,14 @@
+package com.fintech.accountsservice.service.client;
+
+import com.fintech.accountsservice.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient{
+
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
