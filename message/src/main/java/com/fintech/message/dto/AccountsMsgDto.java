a56a0951823a377e98181ef74aadaa319cd65b0e
ShivamWadkar <shivamvwadkar@gmail.com>
2024-01-23 00:05:45 +0530

    Implement & demo of Async communication or event streaming using Kafka

--- message/src/main/java/com/fintech/message/dto/AccountsMsgDto.java
+++ message/src/main/java/com/fintech/message/dto/AccountsMsgDto.java
@@ -1,4 +1,10 @@
 package com.fintech.message.dto;
 
-public record AccountsMsgDto(Long accountNumber, String name, String mobileNumber) {
+/**
+ * @param accountNumber
+ * @param name
+ * @param email
+ * @param mobileNumber
+ */
+public record AccountsMsgDto(Long accountNumber, String name, String email, String mobileNumber) {
 }
--- message/src/main/java/com/fintech/message/functions/MessageFunctions.java
+++ message/src/main/java/com/fintech/message/functions/MessageFunctions.java
@@ -28,4 +28,5 @@ public class MessageFunctions {
             return accountsMsgDto.accountNumber();
         };
     }
+
 }
